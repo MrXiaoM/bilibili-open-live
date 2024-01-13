1a90df97e22828b09551c46d72c6e8af1a348dba
MrXiaoM <coolxiaom95@gmail.com>
2024-01-13 17:40:51 +0800

    add disconnect command

--- src/main/java/top/mrxiaom/bili/bukkit/BukkitMain.java
+++ src/main/java/top/mrxiaom/bili/bukkit/BukkitMain.java
@@ -10,6 +10,7 @@ import top.mrxiaom.bili.bukkit.utils.Util;
 import top.mrxiaom.bili.live.runtime.utils.SignHolder;
 
 import static top.mrxiaom.bili.bukkit.utils.Util.registerPlaceholder;
+import static top.mrxiaom.bili.bukkit.utils.Util.t;
 
 public final class BukkitMain extends JavaPlugin {
     String appId = "";
@@ -48,12 +49,12 @@ public final class BukkitMain extends JavaPlugin {
         if (sender.isOp()) {
             if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
                 reloadConfig();
-                Util.t(sender, "&a配置文件已重载");
+                t(sender, "&a配置文件已重载");
                 return true;
             }
             if (args.length == 1 && args[0].equalsIgnoreCase("debug")) {
                 debug = !debug;
-                return Util.t(sender, "&a调试模式已" + (debug ? "开启" : "关闭"));
+                return t(sender, "&a调试模式已" + (debug ? "开启" : "关闭"));
             }
         }
         if (sender.hasPermission("bili.live")) {
@@ -61,16 +62,20 @@ public final class BukkitMain extends JavaPlugin {
                 if (client != null) client.close();
                 String code = args.length == 2 ? args[1] : defaultCode;
                 Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-                    Util.t(sender, "&7正在连接到开放平台");
+                    t(sender, "&7正在连接到开放平台");
                     try {
                         client = CraftBLiveClient.connect(this, code, appId);
-                        Util.t(sender, "&a连接成功");
+                        t(sender, "&a连接成功");
                     } catch (Throwable t) {
                         t.printStackTrace();
-                        Util.t(sender, "&4连接失败: &c" + t.getMessage(),
+                        t(sender, "&4连接失败: &c" + t.getMessage(),
                                 "&e请联系管理员查阅控制台日志获取详细信息");
                     }
                 });
+            }
+            if (args.length == 1 && args[0].equalsIgnoreCase("close")) {
+                if (client != null) client.close();
+                return t(sender, "&a已断开连接");
             }
         }
         return true;
